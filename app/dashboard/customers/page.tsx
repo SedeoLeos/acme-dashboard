f687fd83c1e03daa8a1bd9ebcbf7390b92ddefb9
King Sedeo Leos <smatsoula19@gmail.com>
2023-10-29 09:46:55 +0100

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,13 @@
+//import liraries
+
+
+// create a component
+const Page = () => {
+    return (
+        <p>Customers Page</p>
+    );
+};
+
+
+//make this component available to the app
+export default Page;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,11 @@
+
+// create a component
+const Page = () => {
+    return (
+       <p>Invoices Page</p>
+    );
+};
+
+
+//make this component available to the app
+export default Page;
